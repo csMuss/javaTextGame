489b5f70b847883632d0c046649979e51195b89f
Nick <chesinick@gmail.com>
2024-04-28 20:47:13 -0600

    Added broken sword item

--- Item/ItemManager.java
+++ Item/ItemManager.java
@@ -1,5 +1,16 @@
 package textGame.Item;
 
+import java.util.ArrayList;
+
+import textGame.Item.Items.BrokenSwordItem;
+
 public class ItemManager {
+	
 	// holds all created items, can add to the player inventory, and remove from
+	
+	public static ArrayList<Item> itemList = new ArrayList<Item>();
+	
+	public ItemManager() {
+		itemList.add(new BrokenSwordItem());
+	}
 }
